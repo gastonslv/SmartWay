2f0267a8834d9cc15b2b63235fb670996d40a0fd
Gaston Silvestre <gastonsilvestrebrc@gmail.com>
2024-11-27 16:31:44 -0300

    verify schedules and an empty options check

--- src/script.js
+++ src/script.js
@@ -4,33 +4,40 @@ const form = document.getElementById("form_configuration");
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    
-    const hour_start = document.getElementById("select_hour_start");
-    const minutes_start = document.getElementById("select_minutes_start");
-    const hour_end = document.getElementById("select_hour_end");
-    const minutes_end = document.getElementById("select_minutes_end");
 
+    // schedules
+    const hour_start = parseInt(document.getElementById("select_hour_start").value);
+    const minutes_start = parseInt(document.getElementById("select_minutes_start").value);
+    const hour_end = parseInt(document.getElementById("select_hour_end").value);
+    const minutes_end = parseInt(document.getElementById("select_minutes_end").value);
+    const valves = parseInt(document.getElementById("select_valves").value);
+    const flow = parseInt(document.getElementById("select_flow").value);
 
-    
-    if (
-        parseInt(hour_start.value) >= parseInt(hour_end.value) && 
-        parseInt(minutes_start.value) >= parseInt(minutes_end.value)
-    ) {
+    if (verifyShcedules(hour_start, minutes_start, hour_end, minutes_end)) {
         
-        if (
-            parseInt(hour_start.value) === parseInt(hour_end.value) &&
-            parseInt(minutes_start.value) === parseInt(minutes_end.value)
-        ) {
-            console.log("No se puede iniciar el riego y finalizarlo a la misma hora.")
+        if (valves == -1 || flow == -1) {
+            alert("Select a valid option in valves and flow rate");
         } else {
-            console.log("No se puede iniciar el riego si usted lo finaliza antes.");
-        }
-    } else {
-        console.log("a");
+            
+            window.location.href = "./pages/valves.html";
+        }   
     }
 
 });
 
-function verifyShchedules() {
+function verifyShcedules(hour_start, minutes_start, hour_end, minutes_end) {
+    const start_time = hour_start * 60 + minutes_start;
+    const end_time = hour_end * 60 + minutes_end;
 
+    if (start_time >= end_time) {
+        if (start_time === end_time) {
+            alert("Irrigation cannot be started and stopped at the same time");
+            return false;
+        } else {
+            alert("Irrigation cannot be started if you terminate it before");
+            return false;
+        }
+    } else {
+        return true;
+    }
 }
